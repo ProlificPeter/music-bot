e7ec056f3af4ca9bff9caea8077f91c86a02727a
Peter Granlund <pgranlund@tactilemedical.com>
2024-06-06 04:24:37 +0000

    Updating files

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ from dotenv import load_dotenv
 
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+# CHANNELFOLLOW = os.getenv('DISCORD_CHANNEL_ID')
 SPOTIFYID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFYSECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 PLAYLISTID = os.getenv('SPOTIFY_PLAYLIST_ID')
+
 
 
 # Default intents are now required to pass to Client
@@ -25,25 +28,29 @@ async def add_song_to_playlist(song_url, playlist_id):
     # Replace <client_id> and <client_secret> with your own Spotify API credentials
     auth = SpotifyOAuth(client_id=SPOTIFYID,
                          client_secret=SPOTIFYSECRET,
-                         redirect_uri='http://localhost:8888/callback',
+                         redirect_uri='http://rldimensions.com:8888/',
+                         open_browser=False,
                          scope=['playlist-modify-public'])
+#    auth = SpotifyClientCredentials(client_secret=SPOTIFYSECRET,
+#                                    client_id=SPOTIFYID)
     sp = spotipy.Spotify(auth_manager=auth)
+    print(auth)
 
     # Extract the song id from the song url
     song_id = song_url.split('track/')[1]
-    song_id = song_id.split('?')[0]
-    song_id = ("spotify:track:" + song_id)
+    track_id = song_id.split('?')[0]
+    song_id = ("spotify:track:" + track_id)
     print(song_id)
+
+    # Getting the song name for message formatting
+    track = sp.track(track_id)
+    track_name = track['name']
+    print(track_name)
 
     # Add the song to the playlist
     # await asyncio.sleep(0)
     sp.playlist_add_items(playlist_id=PLAYLISTID, items=[song_id], position=None)
 
-    # Getting the song name for message formatting
-    track_id = song_id.split(':')[2]
-    track = sp.track(track_id)
-    track_name = track['name']
-    print(track_name)
     return track_name
 
 
@@ -62,6 +69,7 @@ async def on_message(message):
         track_name = await add_song_to_playlist(message.content, PLAYLISTID)
         embed = discord.Embed(title=f"Adding '" + track_name + "' to my playlist.", color=0x00ff00)
         await message.channel.send(embed=embed)
-
+    else:
+        await add_song_to_playlist("https://open.spotify.com/track/3hwbRqQLezmv5Yo2LITSnb?si=613504d3b4ff453e", PLAYLISTID)
 
 client.run(TOKEN)
