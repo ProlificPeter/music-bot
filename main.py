dad053e3ef9262d3b0a9d72ec318b9b43d757913
Peter Granlund <pgranlund@tactilemedical.com>
2024-06-06 16:56:38 -0500

    Updates to add additional discord functionality, added requirements doc.

--- main.py
+++ main.py
@@ -17,41 +17,80 @@ SPOTIFYID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFYSECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 PLAYLISTID = os.getenv('SPOTIFY_PLAYLIST_ID')
 
-
-
 # Default intents are now required to pass to Client
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
+# CONSTANTS
+SPOTIFY_TRACK_URL_HEADER = 'https://open.spotify.com/track/'
 
-async def add_song_to_playlist(song_url, playlist_id):
+async def getHipsterScore(trackPopularity):
+    return 100 - trackPopularity
+
+async def getHipsterRatingText(popularity):
+    hipsterScore = await getHipsterScore(popularity)
+    hipsterRatingText = "This Track has a hipster score of " + str(hipsterScore) + " out of 100."
+    return hipsterRatingText
+
+async def getTrackTitleAndArtist(track):
+    trackTitle = await getAttributeFromTrack(track, 'name')
+    trackArtist = await getAttributeFromTrack(track, "artists")
+    trackTitleAndArtist = trackTitle + " by " + trackArtist
+    return trackTitleAndArtist
+
+async def getTrackDetails(trackId):
+    track_detail_string = "Track: "
+    return track_detail_string
+
+async def getAttributeFromTrack(track, attribute):
+    if attribute == 'artists':
+        return track['artists'][0]["name"]
+    else:
+        return track[attribute]
+
+async def getSpotifyInstance():
     # Replace <client_id> and <client_secret> with your own Spotify API credentials
     auth = SpotifyOAuth(client_id=SPOTIFYID,
                          client_secret=SPOTIFYSECRET,
-                         redirect_uri='http://rldimensions.com:8888/',
-                         open_browser=False,
+                         redirect_uri='http://localhost:8888/callback',
+                         open_browser=True,
                          scope=['playlist-modify-public'])
-#    auth = SpotifyClientCredentials(client_secret=SPOTIFYSECRET,
-#                                    client_id=SPOTIFYID)
     sp = spotipy.Spotify(auth_manager=auth)
-    print(auth)
+    return sp
+
+async def getTrackFromId(trackId):
+    sp = await getSpotifyInstance()
+    track = sp.track(trackId)
+    return track
+
+async def getTrackIdFromUrl(songUrl):
 
     # Extract the song id from the song url
-    song_id = song_url.split('track/')[1]
-    track_id = song_id.split('?')[0]
-    song_id = ("spotify:track:" + track_id)
-    print(song_id)
+    songId = songUrl.split('track/')[1]
+    trackId = songId.split('?')[0]
+    # song_id = ("spotify:track:" + track_id)
+    return trackId
+
+async def add_song_to_playlist(songUrl, playlistId):
+    sp = await getSpotifyInstance()
+
+    # print(song_id)
 
     # Getting the song name for message formatting
-    track = sp.track(track_id)
+    trackId = await getTrackIdFromUrl(songUrl)
+    track = sp.track(trackId)
     track_name = track['name']
-    print(track_name)
+    track_artist = track['artists'][0]["name"]
+    track_hipster_rating = await getHipsterScore(track['popularity'])
+    tmp_message_text_line_one = track_name + " by " + track_artist + " has been added to the Discord Playlist."
+    tmp_message_text_line_two = await getHipsterRatingText(track_hipster_rating)
+    tmp_message_text = tmp_message_text_line_one + "\n" + tmp_message_text_line_two
+    # print(track_name)
+    # print(track_artist)
+    # print(tmp_message_text)
 
-    # Add the song to the playlist
-    # await asyncio.sleep(0)
-    sp.playlist_add_items(playlist_id=PLAYLISTID, items=[song_id], position=None)
 
-    return track_name
+    return tmp_message_text
 
 
 
@@ -65,11 +104,42 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('https://open.spotify.com/track/'):
-        track_name = await add_song_to_playlist(message.content, PLAYLISTID)
-        embed = discord.Embed(title=f"Adding '" + track_name + "' to my playlist.", color=0x00ff00)
+    if message.content.startswith(SPOTIFY_TRACK_URL_HEADER):
+        message_text = await add_song_to_playlist(message.content, PLAYLISTID)
+        embed = discord.Embed(title=message_text, color=0x00ff00)
         await message.channel.send(embed=embed)
     else:
-        await add_song_to_playlist("https://open.spotify.com/track/3hwbRqQLezmv5Yo2LITSnb?si=613504d3b4ff453e", PLAYLISTID)
+        if client.user in message.mentions:
+            if "!hipster" in message.content.lower():
+                cleanedMessage = message.content.split("!hipster ")
+                if len(cleanedMessage) < 2:
+                    # print(cleanedMessage)
+                    return
+                if cleanedMessage[1].startswith(SPOTIFY_TRACK_URL_HEADER):
+                    trackUrl = cleanedMessage[1]
+                    trackId = await getTrackIdFromUrl(trackUrl)
+                    track = await getTrackFromId(trackId)
+                    trackTitleAndArtist = await getTrackTitleAndArtist(track)
+                    popularityRating = await getAttributeFromTrack(track, 'popularity')
+                    hipsterText = await getHipsterRatingText(popularityRating)
+                    responseText = trackTitleAndArtist + ": " + hipsterText
+                    embed = discord.Embed(title=responseText, color=0x00ff00)
+                    await message.channel.send(embed=embed)
+                    # print(message.content)
+                else:
+                    responseText = "Looks like a misformatted request, please make sure to use '!hipster' followed by a space, then the Spotify URL."
+                    embed = discord.Embed(title=responseText, color=0xff0000)
+                    await message.channel.send(embed=embed)
+                    print(message.content)
+            elif "!command" in message.content.lower():
+                embed = discord.Embed(title="!hipster: Find hipster rating of Song based on Spotify URL\n!command: Show list of commands", color=0x0000ff)
+                await message.channel.send(embed=embed)
+                # print("!command fired")
+            else:
+                embed = discord.Embed(title="Hello, still working out some kinks (no shame)", color=0xffff00)
+                await message.channel.send(embed=embed)
+                # print("Failure fired")
+
+
 
 client.run(TOKEN)
